e293a3e66a9c4575b9d1ae1f0ff773e018aa64d1
afdavletkulov <afdavletkulov@rttv.ru>
2025-05-31 13:38:13 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
+// import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
@@ -40,9 +40,4 @@ export default defineConfig({
       },
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
