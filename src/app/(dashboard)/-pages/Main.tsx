6ce42e6a33e337a0e171043df64a5513a37b6a10
Aynur <a.davletkulov@modulbank.ru>
2025-06-05 13:53:55 +0500

    fix

--- src/app/(dashboard)/-pages/Main.tsx
+++ src/app/(dashboard)/-pages/Main.tsx
@@ -1,35 +1,12 @@
-import { useEffect, useState } from 'react'
-import { Page } from '@/components/client/Page'
-import logo from '@/assets/logo.jpeg'
-
 // components
 import { Header } from '@app/-common/components/Header'
 import { Chat } from '@app/-chat/containers/Chat'
 
 const Main = () => {
-  const [showSplash, setShowSplash] = useState(true)
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShowSplash(false)
-    }, 1000) // 1 секунда
-
-    return () => clearTimeout(timeout)
-  }, [])
-
-  if (showSplash) {
-    return (
-      <div className='min-h-screen flex items-center justify-center w-full'>
-        <img src={logo} alt='Logo' className='w-20 h-20 object-contain rounded-[8px]' />
-      </div>
-    )
-  }
-
   return (
     //
     <div>
       <Header />
-
       <Chat />
     </div>
   )
